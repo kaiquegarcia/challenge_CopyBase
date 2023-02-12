d3ed0ad6afa4513196d3d3b45cd78d59c15b5d30
arneto19 <armandopdneto@gmail.com>
2023-02-12 00:08:42 -0300

    Structure adjustments.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,32 @@
-<script setup lang="ts">
-import PokeCard from './components/PokeCard.vue';
-import Search from './components/Search.vue';
+<script lang="ts">
+import PokeCard from "./components/PokeData/PokeCard.vue";
+import Search from "./components/Search/Search.vue";
+
+export default {
+  components: {
+    PokeCard,
+    Search,
+  },
+  data() {
+    return {
+      hasData: false,
+    };
+  },
+  methods: {
+    toggleData() {
+      this.hasData = !this.hasData;
+    },
+  },
+};
 </script>
 
 <template>
   <h1>Pokédex Search</h1>
   <Search />
-  <PokeCard />
+  <button @click="toggleData">Toggle</button>
+  <PokeCard v-if="hasData" />
 </template>
+
+<style lang="scss">
+@import "./style.scss";
+</style>
---
+++ src/components/PokeData/PokeCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+defineProps({
+  data: {},
+});
+</script>
+
+<template>
+  <div :class="'card'"></div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  width: 360px;
+  height: fit-content;
+  border-radius: 12px;
+  background-color: #f1f1f1;
+  margin: 8px auto;
+}
+</style>
